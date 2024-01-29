9631652fa924e6686acc7c46feda32b6e1970003
Yousri <yousreouali@gmail.com>
2024-01-29 22:40:37 +0100

    add images and start json part

--- FileManager.js
+++ FileManager.js
@@ -1,6 +1,72 @@
-class FM{
-    
-    constructor(path){
+import * as FileSystem from 'expo-file-system';
 
+class FM {
+  JSONpath = FileSystem.documentDirectory + 'data.json'; // Chemin vers le fichier JSON
+
+  async add_new_ref(new_subject) {
+    // Première étape, je lis ce qu'il y a dans le json et puis je vérifie et puis la j'écris
+    const currentData = await this.read_file();
+    let updatedData;
+
+    if("default" in currentData){
+        // Clear and make new data
+        this.rewrite_data(new_subject);
+        updatedData = currentData;
+        console.log("ahahahahaah");
+
+    }else{
+        console.log("j'ajoute ici", updatedData);
+        // Add it to file
+        updatedData = [...currentData, new_subject];
+        this.rewrite_data(updatedData);  
     }
-}
+
+    // Et en même temps il me renvoie les données courantes
+    return updatedData
+
+  }
+
+  async read_file() {
+    try {
+      const fileContent = await FileSystem.readAsStringAsync(this.JSONpath);
+      const parsedData = JSON.parse(fileContent);
+
+      return parsedData;
+    } catch (error) {
+      console.error('Erreur lors de la lecture :', error);
+    }
+  }
+
+  async rewrite_data(data){
+
+    new_data = {}
+
+    new_data[data] = [];
+
+    try {
+        await FileSystem.writeAsStringAsync(this.JSONpath, JSON.stringify(new_data));
+      } catch (error) {
+        console.error('Erreur lors de la réécriture des données :', error);
+      }
+    }
+
+
+    async get_all_ref() {
+        let data_to_return = [];
+    
+        try {
+          const fileContent = await FileSystem.readAsStringAsync(this.JSONpath);
+          const parsedData = JSON.parse(fileContent);
+    
+          for (const [key, value] of Object.entries(parsedData)) {
+            data_to_return.push({ id: key, title: key });
+          }
+          return data_to_return;
+        } catch (error) {
+          console.error('Erreur lors de la lecture :', error);
+          return [];
+        }
+      }
+    }
+
+export default FM;
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,62 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, StatusBar, Text, FlatList, SafeAreaView, Dimensions, Modal, Pressable, TextInput, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import { View, StyleSheet, StatusBar, Text, FlatList, SafeAreaView, Dimensions, Modal, Pressable, TextInput, TouchableWithoutFeedback, Keyboard, Image } from 'react-native';
 import { FloatingAction } from 'react-native-floating-action';
 import { useFonts } from 'expo-font';
 import * as FileSystem from 'expo-file-system';
+import FM from "./FileManager";
 
 const screenWidth = Dimensions.get('window').width; // Largeur de l'écran
 
+let DATA = [
+  {
+    id: 'default',
+    title: 'Rien à montrer',
+  }, 
+];
+
+let test = {
+  "ldld" : [{"sourate": 5, "commentaire": "lol"}]
+}
+
+
 const HomeScreen = () => {
 
-  dataToShow = {};
-  /*
+  const [titleText, setTitleText] = useState('Mes sujets');
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [newPotentialSubject, onChangeText] = useState();
+
+  const [data_in_flatlist, setDataInFlatlist] = useState(DATA);
+  
   useEffect(() => {
-    const writeToFile = async () => {
-      const filePath = `${FileSystem.documentDirectory}data.json`; // Fichier qui va contenir les données de toutes les références
-
-      jsonData = {
-        "Santé": {"sourate": 5, "verset": 10},
-        "Vie":{"sourate": 10, "verset": 16},
-      }
-
-      try {
-        // Write to file
-        //await FileSystem.writeAsStringAsync(filePath, JSON.stringify(jsonData));
-
-        // Read from file
-        const fileContent = await FileSystem.readAsStringAsync(filePath);
-        const parsedData = JSON.parse(fileContent);
-
-        //console.log('File content:', parsedData);
-        dataToShow = parsedData;
-        //console.log(`${FileSystem.documentDirectory}data.json`);
-      } catch (error) {
-        console.error('Error accessing file:', error);
-      }
+    const readFile = async () => { 
+      let fileM = new FM();
+      setDataInFlatlist(await fileM.get_all_ref());
     };
 
-    writeToFile();
+    readFile();
   }, []);
-  */
-
-  const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
-
+  
 
   // Fonction qui retourne les composants des items de la flatlist
-  const Item = ({title}) => (
+  const Item = ({ title }) => (
     <View style={styles.item}>
       <Text style={styles.textItem}>{title}</Text>
     </View>
@@ -64,20 +48,25 @@ const HomeScreen = () => {
 
   const dismissKeyboard = () => {
     Keyboard.dismiss(); // Je quitte le clavier
-  }
+  };
 
+  const add_new_ref = async () => {
+    console.log("yoyoyoyoy", newPotentialSubject);
+    let fileM = new FM();
+    
+    fileM.add_new_ref(newPotentialSubject);
+
+    let new_data = await fileM.get_all_ref();
+
+    console.log(new_data);
+
+    setDataInFlatlist(new_data);
+  };
 
   // Set fonts style
   const [fontsLoaded] = useFonts({
     Minecraft: require('./assets/fonts/minecraft_font.ttf'),
   });
-
-  const [titleText, setTitleText] = useState('Mes sujets');
-
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const [newPotentialSubject, onChangeText] = React.useState();
-
 
   const actions = [
     {
@@ -87,10 +76,12 @@ const HomeScreen = () => {
     },
   ];
 
-  // Je n'affiche rien si la polica n'a pas été chargé
+  // Je n'affiche rien si la police n'a pas été chargé
   if (!fontsLoaded) {
-    return null; 
+    return null;
   }
+
+  // console.log(data_in_flatlist);
 
   return (
     <View style={styles.MainContainer}>
@@ -103,8 +94,8 @@ const HomeScreen = () => {
 
       <SafeAreaView style={styles.ListContainer}>
         <FlatList
-          data={DATA}
-          renderItem={({item}) => <Item title={item.title} />}
+          data={data_in_flatlist}
+          renderItem={({ item }) => <Item title={item.title} />}
           keyExtractor={item => item.id}
         />
       </SafeAreaView>
@@ -114,6 +105,7 @@ const HomeScreen = () => {
         actions={actions}
         onPressItem={(name) => {
           if (name === 'new_subject') {
+            onChangeText(""); // J'initialise le texte
             setModalVisible(!modalVisible);
           }
         }}
@@ -127,25 +119,56 @@ const HomeScreen = () => {
       <Modal
         animationType="slide"
         visible={modalVisible}
-        >
+      >
         <TouchableWithoutFeedback onPress={dismissKeyboard}>
-        <View style={styles.continerInModal}>
-          <View style={styles.containerTextInModal1}>
-            <Text style={styles.textItem}>Nouveau sujet :</Text>
-            <SafeAreaView>
-              <TextInput
-                style={styles.input}
-                onChangeText={onChangeText}
-                placeholder="Saisissez le nom du sujet"
-              />
-            </SafeAreaView>
-            <Pressable
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.buttonCloseModal} >Ajouter ce sujet</Text>
-            </Pressable>
-            
+          <View style={styles.continerInModal}>
+            <Image
+              source={require('./assets/topIMG.png')}
+              style={[styles.topImage, { transform: [{ scaleY: -1 }] }]}
+              resizeMode="cover"
+            />
+            <View style={styles.containerTextInModal1}>
+              <Text style={styles.textItem}>Nouveau sujet :</Text>
+              <SafeAreaView>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={onChangeText}
+                  placeholder="Saisissez le nom du sujet"
+                  placeholderTextColor="#94743d" 
+                />
+              </SafeAreaView>
+
+              <SafeAreaView style={{flexDirection: "row"}}>
+
+              <Pressable
+                onPress={() => {
+                  // J'appel la fonction qui permet de créer une nouvelle réf
+                  if(newPotentialSubject != ""){
+                    add_new_ref();
+                  }
+                  setModalVisible(!modalVisible); // Je fait disparaitre le modal
+                }}>
+                <Text style={styles.buttonCloseModal} >Ajouter ce sujet</Text>
+              </Pressable>
+
+              <Pressable
+                onPress={() => {
+                  setModalVisible(!modalVisible); // Je fait juste disparaitre le modal
+                }}>
+                <Text style={styles.buttonCloseModal2} >Annuler</Text>
+              </Pressable>
+
+              </SafeAreaView>
+
+            </View>
+
+            <Image
+              source={require('./assets/bottomIMG.png')}
+              style={styles.bottomImage}
+              resizeMode="cover"
+            />
+
           </View>
-        </View>
         </TouchableWithoutFeedback>
 
       </Modal>
@@ -180,7 +203,7 @@ const styles = StyleSheet.create({
     flex: 1,
     //backgroundColor: 'gray',
     alignItems: 'center',
-    alignSelf:'stretch',
+    alignSelf: 'stretch',
   },
   textItem: {
     fontSize: 25,
@@ -189,35 +212,76 @@ const styles = StyleSheet.create({
   item: {
     backgroundColor: '#e1dd72',
     padding: 10, // Créer plus d'espace pour l'item
-    width: screenWidth-50,
+    width: screenWidth - 50,
     marginVertical: 8, // espace entre les items
-    borderWidth:  1, // épaisseur des bordures
-    borderRadius:  20, // rayon des bordures
+    borderWidth: 1, // épaisseur des bordures
+    borderRadius: 20, // rayon des bordures
     alignItems: 'center',
   },
   continerInModal: {
-    flex: 1,    
+    flex: 1,
     alignItems: 'center',
-    alignSelf:'stretch',
+    alignSelf: 'stretch',
   },
   containerTextInModal1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    // backgroundColor: 'gray',
+    alignSelf: 'stretch',
+    // marginTop: 250,
   },
   input: {
-    width: screenWidth-60,
+    width: screenWidth - 60,
     borderWidth: 1,
     padding: 20,
     marginTop: 40,
+    fontFamily: 'Minecraft',
+    textAlign: 'center',
   },
   buttonCloseModal: {
-    top: 50,
+    top: 30,
     fontFamily: 'Minecraft',
     fontSize: 20,
     textDecorationStyle: 'dashed',
+    backgroundColor: '#509110',
+    borderRadius: 10,
+    borderWidth: 2,
+    overflow: 'hidden', // => TROP BIEN, ca permet de supprimer le surplus du background color
+    padding: 3,
   },
+  buttonCloseModal2: {
+    top: 30,
+    fontFamily: 'Minecraft',
+    fontSize: 20,
+    textDecorationStyle: 'dashed',
+    backgroundColor: '#872000',
+    borderRadius: 10,
+    borderWidth: 2,
+    overflow: 'hidden',
+    padding: 3,
+    marginHorizontal: 5
+  },
+
+
+  bottomImage: { // Je designe une position absolue pour les images
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: screenWidth,
+    height: 300,  // Ajustez la hauteur selon vos besoins
+  },
+  topImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: screenWidth,
+    height: 400,  // Ajustez la hauteur selon vos besoins
+
+  }
+
 });
 
 export default HomeScreen;
